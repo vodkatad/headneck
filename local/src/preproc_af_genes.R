9caa9e4dabd7a48ac7c746bb014c9c2ec20a2c8f
Elena Grassi <grassi.e@gmail.com>
2022-04-05 10:14:44 +0000

    Preprocessing MAF and first oncoprint attempt

---
+++ local/src/preproc_af_genes.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+
+xeno_af <- snakemake@input[['matX']]
+human_af <- snakemake@input[['matH']]
+log_f <- snakemake@log[['log']]
+
+savedata <- snakemake@output[['preprocAF']]
+
+thr <- as.numeric(snakemake@wildcards[['AF']])
+
+xeno <- read.table(xeno_af, sep="\t", header=T, row.names=1)
+human <- read.table(human_af, sep="\t", header=T, row.names=1)
+
+# o takes the h place
+pdobing <- ifelse(human > thr, TRUE, FALSE)
+pdxbing <- ifelse(xeno > thr, TRUE, FALSE)
+
+xenos <- substr(colnames(xeno), 0, 7)
+human <- substr(colnames(human), 0, 7)
+common <- intersect(human, xenos)
+
+sink(log_f)
+print('human')
+length(human)
+print('xenos')
+length(xenos)
+print('common')
+length(common)
+sink()
+
+colnames(pdobing) <- substr(colnames(pdobing), 0, 7)
+colnames(pdxbing) <- substr(colnames(pdxbing), 0, 7)
+
+pdobing <- pdobing[, common]
+pdxbing <- pdxbing[, common]
+
+save.image(savedata)
